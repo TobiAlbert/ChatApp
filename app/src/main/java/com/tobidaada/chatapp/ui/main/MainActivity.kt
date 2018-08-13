f703173b6868960d4b2fe9cb2120162e5b6dd5ee
Tobi Daada <daadatobi@gmail.com>
2018-08-14 00:03:51 +0100

    Add Toolbar and MenuOptions

--- app/src/main/java/com/tobidaada/chatapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/tobidaada/chatapp/ui/main/MainActivity.kt
@@ -1,24 +1,32 @@
 package com.tobidaada.chatapp.ui.main
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
 import com.google.firebase.auth.FirebaseAuth
 import com.tobidaada.chatapp.R
 import com.tobidaada.chatapp.ui.start.StartActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
 
     private lateinit var mAuth: FirebaseAuth
+    private lateinit var mToolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         mAuth = FirebaseAuth.getInstance()
-    }
+        mToolbar = main_toolbar as Toolbar
 
+        setSupportActionBar(mToolbar)
+        supportActionBar?.title = "ChatApp"
+    }
 
     override fun onStart() {
         super.onStart()
@@ -27,10 +35,44 @@ class MainActivity : AppCompatActivity() {
         val currentUser = mAuth.currentUser
 
         if (currentUser == null) {
-            val startIntent = Intent(this@MainActivity, StartActivity::class.java)
-            startActivity(startIntent)
-            finish()
+            sendToStartActivity()
         }
 
     }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.logout_menu -> logoutUser()
+            R.id.all_users_menu -> getAllUsers()
+            R.id.account_settings_menu -> accountSettings()
+
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun sendToStartActivity() {
+        val startIntent = Intent(this@MainActivity, StartActivity::class.java)
+        startActivity(startIntent)
+        finish()
+    }
+
+    private fun logoutUser() {
+        mAuth.signOut()
+
+        val currentUser = mAuth.currentUser
+        if (currentUser == null) {
+            sendToStartActivity()
+        }
+    }
+
+    private fun getAllUsers() {}
+
+    private fun accountSettings() {}
 }
