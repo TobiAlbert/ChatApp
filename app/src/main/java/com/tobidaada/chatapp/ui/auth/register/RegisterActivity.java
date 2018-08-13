9d5519b18d80a0a2e1f52b2724df799490e882dd
Tobi Daada <daadatobi@gmail.com>
2018-08-14 00:01:15 +0100

    Move RegisterActivity.java file

--- app/src/main/java/com/tobidaada/chatapp/ui/auth/register/RegisterActivity.java
+++ app/src/main/java/com/tobidaada/chatapp/ui/auth/register/RegisterActivity.java
@@ -1,10 +1,13 @@
-package com.tobidaada.chatapp.ui.auth;
+package com.tobidaada.chatapp.ui.auth.register;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -14,7 +17,6 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.tobidaada.chatapp.R;
 import com.tobidaada.chatapp.ui.main.MainActivity;
 
@@ -25,6 +27,8 @@ public class RegisterActivity extends AppCompatActivity {
     private TextInputEditText mPasswordEdit;
     private Button mRegisterButton;
     private FirebaseAuth mAuth;
+    private Toolbar mToolbar;
+    private ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +41,14 @@ public class RegisterActivity extends AppCompatActivity {
         mEmailEdit = findViewById(R.id.regsiter_email_et);
         mPasswordEdit = findViewById(R.id.register_password_et);
         mRegisterButton = findViewById(R.id.register_btn);
+        mToolbar = findViewById(R.id.register_toolbar);
+        mProgressDialog = new ProgressDialog(this);
+
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Create Account");
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         mRegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,7 +58,16 @@ public class RegisterActivity extends AppCompatActivity {
                 String email = mEmailEdit.getText().toString().trim();
                 String password = mPasswordEdit.getText().toString().trim();
 
-                registerUser(displayName, email, password);
+                if (!TextUtils.isEmpty(displayName) && !TextUtils.isEmpty(email) &&
+                        !TextUtils.isEmpty(password)) {
+
+                    mProgressDialog.setTitle("Registering User");
+                    mProgressDialog.setMessage("Please Wait while Creating Account");
+                    mProgressDialog.setCancelable(false);
+                    mProgressDialog.show();
+
+                    registerUser(displayName, email, password);
+                }
 
             }
         });
@@ -59,6 +80,7 @@ public class RegisterActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
+                            mProgressDialog.dismiss();
                             // Sign in success, update UI with the signed-in user's information
                             startActivity(new Intent(RegisterActivity.this, MainActivity.class));
                             finish();
@@ -67,6 +89,7 @@ public class RegisterActivity extends AppCompatActivity {
                             Log.i("ChatApp", "Task Result: " + task.getResult().toString());
                             Toast.makeText(getApplicationContext(), "Authentication Failed", Toast.LENGTH_SHORT)
                                     .show();
+                            mProgressDialog.hide();
                         }
                     }
                 });
